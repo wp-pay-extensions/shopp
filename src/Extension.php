d8eaf7b4889387c2b8dda34bb66d1e417d37af4b
Remco Tolsma <info@remcotolsma.nl>
2017-01-25 14:42:36 +0100

    Getting ready for version 1.0.6.

--- src/Extension.php
+++ src/Extension.php
@@ -3,11 +3,11 @@
 /**
  * Title: WordPress pay extension Shopp
  * Description:
- * Copyright: Copyright (c) 2005 - 2016
+ * Copyright: Copyright (c) 2005 - 2017
  * Company: Pronamic
  *
  * @author Remco Tolsma
- * @version 1.0.0
+ * @version 1.0.6
  * @since 1.0.0
  */
 class Pronamic_WP_Pay_Extensions_Shopp_Extension {
